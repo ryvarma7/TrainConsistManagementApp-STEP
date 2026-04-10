2e3123a333c20f421abdda3b69a0ee86716b21f5
ryvarma7 <ryvarma7@gmail.com>
2026-04-10 16:01:29 +0530

    Added: UC8

--- TrainManagmentapp.java
+++ TrainManagmentapp.java
@@ -1,11 +1,33 @@
 import java.util.*;
+import java.util.stream.*;
+
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+}
 
 public class TrainManagmentapp {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
-        
-        List<String> bogies = new ArrayList<>();
-        
-        System.out.println("Initial bogie count: " + bogies.size());
+
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 60));
+        bogies.add(new Bogie("First Class", 24));
+        bogies.add(new Bogie("Luxury", 80));
+
+        List<Bogie> filtered = bogies.stream()
+                .filter(b -> b.capacity > 60)
+                .collect(Collectors.toList());
+
+        for (Bogie b : filtered) {
+            System.out.println(b.name + " Capacity: " + b.capacity);
+        }
     }
 }
