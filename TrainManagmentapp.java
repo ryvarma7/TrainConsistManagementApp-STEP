4bb1caef9460e2364208c704b543f1dee317e17d
ryvarma7 <ryvarma7@gmail.com>
2026-04-10 15:51:14 +0530

    Added: UC5

--- TrainManagmentapp.java
+++ TrainManagmentapp.java
@@ -1,11 +1,17 @@
 import java.util.*;
 
-public class TrainManagmentapp {
+public class TrainManagmentappUC1toUC8 {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
         
-        List<String> bogies = new ArrayList<>();
+        LinkedHashSet<String> formation = new LinkedHashSet<>();
         
-        System.out.println("Initial bogie count: " + bogies.size());
+        formation.add("Engine");
+        formation.add("Sleeper");
+        formation.add("Cargo");
+        formation.add("Guard");
+        formation.add("Sleeper");
+        
+        System.out.println("Train Formation: " + formation);
     }
 }
